092f4b5502ca260494bbf5ea1b43daf975fb0a9a
Navin Rai <rai.navinrai20@gmail.com>
2025-06-26 21:58:28 +0530

    Initial Commit

---
+++ Leaf Lens/Leaf_LensApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leaf_LensApp.swift
+//  Leaf Lens
+//
+//  Created by Navin Rai on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Leaf_LensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
